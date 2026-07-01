f87b54f93b243de7bf5e7cef52a0e07dd66a399a
Ali El-husseiny <ali.ahmed.software.engineer@gmail.com>
2026-07-02 02:48:22 +0300

    Create 2807-ExecuteAsynchronousFunctionsInParallel

---
+++ https:/github.com/AliEl-Husseiny/Problem-Solving/LeetCode/2807-ExecuteAsynchronousFunctionsInParallel/2807-ExecuteAsynchronousFunctionsInParallel.js
@@ -0,0 +1,30 @@
+// Last updated: 7/2/2026, 2:48:21 AM
+/**
+ * @param {Array<Function>} functions
+ * @return {Promise<any>}
+ */
+var promiseAll = function(functions) {
+    return new Promise((resolve, reject) => {
+        const results = [];
+        let completed = 0;
+
+        functions.forEach((fn, index) => {
+            fn()
+                .then((result) => {
+                    results[index] = result; // Store the result in the same order
+                    completed += 1;
+                    if (completed === functions.length) {
+                        resolve(results); // Resolve when all promises are fulfilled
+                    }
+                })
+                .catch((error) => {
+                    reject(error); // Reject if any promise fails
+                });
+        });
+    });
+};
+
+/**
+ * const promise = promiseAll([() => new Promise(res => res(42))])
+ * promise.then(console.log); // [42]
+ */
