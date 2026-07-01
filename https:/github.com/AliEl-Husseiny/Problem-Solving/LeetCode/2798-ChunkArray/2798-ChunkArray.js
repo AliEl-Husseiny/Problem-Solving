5fee4f37bbd54cec8a46e0a7c8bfd7eda48efd6b
Ali El-husseiny <ali.ahmed.software.engineer@gmail.com>
2026-07-02 02:48:25 +0300

    Create 2798-ChunkArray

---
+++ https:/github.com/AliEl-Husseiny/Problem-Solving/LeetCode/2798-ChunkArray/2798-ChunkArray.js
@@ -0,0 +1,3 @@
+// Last updated: 7/2/2026, 2:48:25 AM
+/**************************\U0001f60e**************************/
+const chunk=(e,l)=>{let t=[];for(;e.length>0;){let c=e.slice(0,l),n=e.slice(l);e=n,t.push(c)}return t};
