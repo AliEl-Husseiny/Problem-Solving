5122faf18539736217182ba121f28dc048ae26d1
Ali El-husseiny <ali.ahmed.software.engineer@gmail.com>
2026-07-02 02:48:10 +0300

    Create 2862-IntervalCancellation

---
+++ https:/github.com/AliEl-Husseiny/Problem-Solving/LeetCode/2862-IntervalCancellation/2862-IntervalCancellation.js
@@ -0,0 +1,8 @@
+// Last updated: 7/2/2026, 2:48:10 AM
+var cancellable = function(fn, args, t) {
+    fn(...args);
+    let timer = setInterval(() => fn(...args), t);
+
+    let cancelFn = () => clearInterval(timer);
+    return cancelFn;
+};
