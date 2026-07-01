9caa79ca373ac1bee85ec6945e1f9d59dd654bc6
Ali El-husseiny <ali.ahmed.software.engineer@gmail.com>
2026-07-02 02:48:45 +0300

    Create 2733-Sleep

---
+++ https:/github.com/AliEl-Husseiny/Problem-Solving/LeetCode/2733-Sleep/2733-Sleep.js
@@ -0,0 +1,12 @@
+// Last updated: 7/2/2026, 2:48:44 AM
+/**
+ * @param {number} millis
+ */
+async function sleep(millis) {
+    await new Promise(resolve => setTimeout(resolve, millis));
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
