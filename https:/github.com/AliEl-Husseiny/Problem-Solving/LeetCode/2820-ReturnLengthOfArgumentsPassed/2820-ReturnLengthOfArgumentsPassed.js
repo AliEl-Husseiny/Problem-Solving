13f2de7e7a162d524071ed6dcc9c6b6669485ab5
Ali El-husseiny <ali.ahmed.software.engineer@gmail.com>
2026-07-02 02:48:16 +0300

    Create 2820-ReturnLengthOfArgumentsPassed

---
+++ https:/github.com/AliEl-Husseiny/Problem-Solving/LeetCode/2820-ReturnLengthOfArgumentsPassed/2820-ReturnLengthOfArgumentsPassed.js
@@ -0,0 +1,12 @@
+// Last updated: 7/2/2026, 2:48:16 AM
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
